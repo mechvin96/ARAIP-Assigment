5bd5fffa483f5a37de4e78e405c6f8ae5deb2156
mechvin96 <cnavindran@gmail.com>
2026-03-05 02:28:50 +0800

    Update Code.py
    try out 3/4 vision of the camera then centre pixel detection for more sensitive the image but it cause lag and software  crashing . So I did a 8 step timer 10 second vision to detect the pixel so it will not lag and camera will be sensitive

--- Code.py
+++ Code.py
@@ -36,6 +36,7 @@ camera = robot.getDevice('camera')
 camera.enable(TIME_STEP)
 cam_width = camera.getWidth()
 cam_height = camera.getHeight()
+print("Camera Initialized and enabled") # Debug Statement(trying out something new)
 
 # Tracking Variables for Color Recognition
 found_status = {'red': False, 'green': False, 'blue': False}
@@ -51,39 +52,67 @@ def print_summary():
 
 # Wandering counter
 wander_counter = 0
+#Camera Timing to make the software not lag
+vision_timer = 0 
 
 # Main Simulation Loop
 while robot.step(TIME_STEP) != -1:
-    # --- 1. VISION LOGIC (RGB Sampling) ---
-    image = camera.getImage()
+
+    #  1. VISION LOGIC (Lag Free Sampling)
+    #  Only run vision every 10 step to maintain performance and reduce lag
+    vision_timer += 1
+    if camera and vision_timer % 10 == 0:
+        image = camera.getImage()
     
-    if image:
+        if image:
+            red_count = 0
+            green_count = 0
+            blue_count = 0
+            total_samples = 0
         # Sample the center pixel
-        cx, cy = cam_width // 2, cam_height // 2
+        #cx, cy = cam_width // 2, cam_height // 2
         
+        # Use a step of 8 to sample  the frame sparsely and reduce lag
+            step = 8
+            for x in range(0, cam_width, step):
+                for y in range(0, cam_height, step):
+                    total_sample +=1
+                    r = camera.imageGetRed(image, cam_width, x, y)
+                    g = camera.imageGetGreen(image, cam_width, x, y)
+                    b = camera.imageGetBlue(image, cam_width, x, y)
+
+                    #Use a specified threshold to count colors, this is more robust than checking a single pixel
+                    if r > 90 and g < 75 and b < 75:
+                        red_count += 1
+                    elif g > 90 and r < 75 and b < 75:
+                        green_count += 1
+                    elif b > 90 and r < 75 and g < 75:
+                        blue_count += 1
+
+
         # Get RGB values
-        r = camera.imageGetRed(image, cam_width, cx, cy)
-        g = camera.imageGetGreen(image, cam_width, cx, cy)
-        b = camera.imageGetBlue(image, cam_width, cx, cy)
+        #r = camera.imageGetRed(image, cam_width, cx, cy)
+        #g = camera.imageGetGreen(image, cam_width, cx, cy)
+        #b = camera.imageGetBlue(image, cam_width, cx, cy)
         
         # OPTIONAL: Uncomment the line below to debug actual RGB values in console
-        print(f"RGB: ({r}, {g}, {b})")
+        #print(f"RGB: ({r}, {g}, {b})")
 
-        detected_color = None
+            detected_color = None
         # Thresholds lowered to 160 for better detection in different lighting
-        if r > 100 and g < 90 and b < 90:
-            detected_color = 'red'
-        elif g > 100 and r < 90 and b < 90:
-            detected_color = 'green'
-        elif b > 100 and r < 90 and g < 90:
-            detected_color = 'blue'
+            if r > 100 and g < 90 and b < 90:
+                detected_color = 'red'
+            elif g > 100 and r < 90 and b < 90:
+                detected_color = 'green'
+            elif b > 100 and r < 90 and g < 90:
+                detected_color = 'blue'
 
         # Check if this is a new discovery
-        if detected_color and not found_status[detected_color]:
-            print(f"I See {detected_color}!")
-            found_status[detected_color] = True
-            found_history.append(detected_color)   
-            print_summary()
+            if detected_color and not found_status[detected_color]:
+                print(f"I See {detected_color}!")
+                found_status[detected_color] = True
+                found_history.append(detected_color)   
+                print_summary()
 
     # --- 2. NAVIGATION LOGIC (Obstacle Avoidance & Wandering) ---
     ps_values = [sensor.getValue() for sensor in ps]
